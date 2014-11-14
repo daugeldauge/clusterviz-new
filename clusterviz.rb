bacc15e1f6870f868e565d178a3f04189f6715a7
Artem Daugel-Dauge <daugeldauge@gmail.com>
2014-11-14 23:39:32 +0300

    Provide simple levels visualization..

--- clusterviz.rb
+++ clusterviz.rb
@@ -2,15 +2,87 @@ require 'open3'
 require 'json'
 require 'sinatra'
 require 'sinatra/contrib'
+require 'sinatra/base'
 require 'timeout'
+require 'neography'
+require 'set'
+
+def get_all_nodes
+  nodes = []
+  $neo.execute_query("MATCH (n) RETURN n")['data'].each do |node|
+    nodes[node[0]['metadata']['id']] = node[0]['data']
+  end
+  nodes
+end
 
 configure do
-  #raise 'Compilation error' unless system 'make -C src/'
-  #set :show_exceptions => false
+  puts 'before starts'
+  $neo = Neography::Rest.new('http://graphit.parallel.ru:7474')
+  $nodes = get_all_nodes()
+  puts 'before ends'
 end
 
 def error_page msg
   erb :error, :locals => {:msg => msg}
+end
+
+def get_roots(type)
+  query = $neo.execute_query("MATCH (n) WHERE (n)-[:#{type}]->() AND NOT ()-[:#{type}]->(n) RETURN n")
+  query['data'].map do |node|
+    node[0]['metadata']['id']
+  end
+end
+
+def get_id(url)
+  url.gsub(/.*\D/, '').to_i
+end
+
+def get_levels(type, levels)
+  roots = get_roots(type)
+  rels = []
+  rels[0] = roots.map{ |root| $neo.get_node_relationships(root, 'out', 'TYPE_0') }.reduce(:+)
+  (0...levels).each do |i|
+    rels[i + 1] = []
+    rels[i].each do |rel|
+      target = get_id rel['end']
+      rels[i + 1] += $neo.get_node_relationships(target, 'out', 'TYPE_0')
+      #puts "#{i + 1} #{rels[i + 1].size}"
+    end
+    #puts "#{i} #{rels[i].size}"
+  end
+  rels
+end
+
+get '/nodes/:id' do
+  content_type :json
+  $nodes[params[:id].to_i].to_json
+end
+
+get '/neo' do
+  levels = get_levels('TYPE_0', 1)
+  rels = levels.reduce(:+)
+  links = rels.map do |rel|
+    {
+      source: get_id(rel['start']),
+      target: get_id(rel['end']),
+      type: rel['data']['type']
+    }
+  end
+
+  i = 0
+  nodes = Set.new
+  links.each do |link|
+    nodes.add(link[:source])
+    nodes.add(link[:target])
+  end
+
+  nodes = nodes.to_a
+  nodes.map! do |node|
+    {id: node, size: 10, type: $nodes[node]['type']}
+  end
+  
+  content_type :json
+  {nodes: nodes, links: links}.to_json
 end
 
 get '/' do
@@ -25,20 +97,4 @@ get '/' do
     dot_info[cluster.to_sym] = info
   end
   erb :start
-end
-
-get '/json' do
-  headers 'Content-Type' => 'application\json'
-  {
-    name: 'hello',
-    children: [
-      {name: 'n1', size: 10000},
-      {name: 'n2', size: 20000},
-      {name: 'n3', size: 10000},
-      {name: 'n4', size: 10000},
-      {name: 'n5', size: 30000},
-      {name: 'n6', size: 10000},
-      {name: 'n7', size: 10000},
-    ]
-  }.to_json
 end
--- public/js/viz.js
+++ public/js/viz.js
@@ -1,6 +1,6 @@
 
 var width = 960,
-    height = 500;
+    height = 960;
 
 var color = d3.scale.category20();
 
@@ -9,14 +9,32 @@ var radius = d3.scale.sqrt()
 
 var svg = d3.select("body").append("svg")
     .attr("width", width)
-    .attr("height", height);
+    .attr("height", height)
+    .style("background", "lightgrey");
 
 var force = d3.layout.force()
     .size([width, height])
     .charge(-400)
     .linkDistance(function(d) { return radius(d.source.size) + radius(d.target.size) + 20; });
 
-d3.json("arctic.json", function(graph) {
+d3.json("/neo", function(graph) {
+    //console.log(graph);
+    graph.index = [];
+    for (var i = 0; i < graph.nodes.length; ++i) {
+        graph.index[graph.nodes[i].id] = i;
+    }
+    //console.log(graph.index);
+    // for (var i = 1; i < graph.links.length; ++i) {
+    //     graph.links[i].source = graph.index[graph.links[i].source];
+    //     graph.links[i].target = graph.index[graph.links[i].source];
+    // }
+    //console.log(graph.links);
+    graph.links.forEach(function (link) {
+        link.source = graph.index[link.source];
+        link.target = graph.index[link.target];
+        //console.log(link);
+    });
+
     force
         .nodes(graph.nodes)
         .links(graph.links)
@@ -29,25 +47,24 @@ d3.json("arctic.json", function(graph) {
         .attr("class", "link");
 
     link.append("line")
-        .style("stroke-width", function(d) { return (d.bond * 2 - 1) * 2 + "px"; });
-
-    link.filter(function(d) { return d.bond > 1; }).append("line")
-        .attr("class", "separator");
+        .style("stroke-width", "1.5px");
 
     var node = svg.selectAll(".node")
         .data(graph.nodes)
         .enter().append("g")
         .attr("class", "node")
+        .on("click", click)
+        .on("dblclick", dblclick)
         .call(force.drag);
 
     node.append("circle")
         .attr("r", function(d) { return radius(d.size); })
-        .style("fill", function(d) { return color(d.atom); });
+        .style("fill", function(d) { return color(d.type); });
 
     node.append("text")
         .attr("dy", ".35em")
         .attr("text-anchor", "middle")
-        .text(function(d) { return d.atom; });
+        .text(function(d) { return d.type; });
 
     function tick() {
         link.selectAll("line")
@@ -57,5 +74,25 @@ d3.json("arctic.json", function(graph) {
             .attr("y2", function(d) { return d.target.y; });
 
         node.attr("transform", function(d) { return "translate(" + d.x + "," + d.y + ")"; });
+    }
+
+    function click(d) {
+        $.getJSON("/nodes/" + d.id, function(node) {
+            var table = $("#node-info")
+                .empty()
+                .css("text-align", "left")
+                //.attr("border", 0.1)
+                .append("<thead><tr><th>Key</th><th>Value</th></tr></thead>")
+                .append("<tr><td>id</td><td>" + d.id +"</td></tr>");
+
+            $.each(node, function(key, value) {
+                table.append("<tr><td>" + key + "</td><td>" + value +"</td></tr>");
+            });
+            table.append("</table>");
+        });
+    }
+
+    function dblclick(d) {
+       alert("dblclick on " + d.id);
     }
 });
